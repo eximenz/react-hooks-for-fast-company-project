2bd12c58f6c0847094cd2d08602c6c1943a01b9c
eximenz <terpigoreva1992@gmail.com>
2022-01-30 19:17:34 +0300

    added reactChildren hook

--- src/app/components/exercises/childrenExercise.jsx
+++ src/app/components/exercises/childrenExercise.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 import CollapseWrapper from "../common/collapse";
+import PropTypes from "prop-types";
+
+// React.Children.map
+const ListComponent = ({ children }) => {
+    return React.Children.map(children, (child, index) => {
+        return (
+            <div className="d-flex">
+                <span className="mx-2">{index + 1}.</span>
+                <div>{child}</div>
+            </div>
+        );
+    });
+};
+
+// React.Children.toArray
+// const ListComponent = ({ children }) => {
+//     const childArray = React.Children.toArray(children);
+//     return (
+//         <>
+//             {childArray.map((child, index) => {
+//                 return (
+//                     <div className="d-flex" key={index}>
+//                         <span className="mx-2">{index + 1}.</span>
+//                         <div>{child}</div>
+//                     </div>
+//                 );
+//             })}
+//         </>
+//     );
+// };
+
+ListComponent.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
+
 const ChildrenExercise = () => {
     return (
         <CollapseWrapper title="Упражнение">
@@ -10,10 +48,11 @@ const ChildrenExercise = () => {
                 <code>React.Children.map</code> так и{" "}
                 <code>React.Children.toArray</code>
             </p>
-
-            <Component />
-            <Component />
-            <Component />
+            <ListComponent>
+                <Component />
+                <Component />
+                <Component />
+            </ListComponent>
         </CollapseWrapper>
     );
 };
